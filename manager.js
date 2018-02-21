73a8575a3aec12aa639aaf8ad16a6062bc2fbca9
Craig Christensen <ruffcorn@gmail.com>
2018-02-21 16:07:52 -0600

    View inventory and View low inventory added

--- manager.js
+++ manager.js
@@ -1,13 +1,14 @@
 // REQUIRED MODULES
 const inquirer = require('inquirer');
-require("dotenv").config();
 const mysql = require('mysql');
 const colors = require('colors');
 const cliBoxes = require('cli-boxes');
+const columnify = require('columnify')
+require("dotenv").config();
 var mysql_pwd = process.env.MYSQL_PASSWORD;
 
 // GLOBALS
-var query_str = '';
+
 
 // create a connection database
 var connection = mysql.createConnection({
@@ -21,15 +22,40 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   // open welcome screen
-  selectDept();
+  mainMenu();
 });
 
-selectDept = function(){
+mainMenu = function(){
+  // clearScreen();
+  displayLogo('main');
+  inquirer
+  .prompt([
+    {
+      name: 'main',
+      type: 'list',
+      message: 'Manager Options',
+      choices: [
+        'View Products',
+        'View Low Inventory',
+        'Add Inventory',
+        'Add New Product',
+        'QUIT'
+      ]
+    }
+  ])
+  .then(function(selection){
+    if(selection.main === 'QUIT'){process.exit()}
+    // parameter will be 'View' or 'Add'
+    var mgrParm = selection.main.split(" ");
+    // console.log(mgrParm[0]);
+    selectDept(mgrParm[0], mgrParm[1]);  
+  });
+}
+
+selectDept = function(operation, target){
   connection.query("SELECT DISTINCT department_name FROM departments", function(err,res){
-    // display Orinoco 'logo'
-    displayLogo();
-    // get user input
-    // begin with choosing a department to narrow items displayed
+    displayLogo('dept');
+    // get user input - begin with choosing a department to narrow items displayed
     inquirer
     .prompt([
       {
@@ -38,7 +64,11 @@ selectDept = function(){
         choices: function(){          
           var deptArr = [];
           var dept;
-          deptArr.push('ALL')
+          deptArr.push('MAIN MENU');
+          // give user option of all items when viewing but narrow by department when adding
+          if(operation === 'View'){
+            deptArr.push('ALL');
+          }
           for (i=0; i<res.length; i++){
             deptArr.push(res[i].department_name);
           }
@@ -49,60 +79,225 @@ selectDept = function(){
       }
     ])
     .then(function(selection){
-      if(selection.dept === 'QUIT'){process.exit()};
-      selectItems(selection.dept);  
+
+      if(selection.dept === 'QUIT'){
+        process.exit()
+      }
+      else if(selection.dept === 'MAIN MENU'){
+        mainMenu();
+      }
+
+      switch(target){
+        case 'Products':
+          getInventory(selection.dept, 'view');
+          break;
+        case 'Low':
+          getInventory(selection.dept, 'low');
+         break;
+        case 'Inventory':
+          addInventory();
+          break;
+        case 'New':
+          addNewProd();
+          break;
+        default:
+          console.log('unknown input');
+          break;
+      }
     });
   });
 }
 
-function selectItems(dept){
-  console.log(dept);
+function getInventory(dept, operation){
+  var response = queryItems(dept, operation);
+  // console.log('reponse returned by queryItems is: '+response)
+  // displayItems(response, operation);  
+}
+
+function addInventory(dept){
   if(dept === 'ALL'){
     connection.query("SELECT * FROM products", function(err,res){
       if (err) throw err;
-      displayItems(res);
+      // displayItems(res, 'view');
     });
   } 
   else{
     connection.query("SELECT * FROM products WHERE department_name = ?", dept, function(err,res){
       if (err) throw err;
-      displayItems(res);
+      // displayItems(res, 'view');
     });
-  }  
-}
-
-function displayItems(res){
-  console.log(res);
-  displayLogo();
+  }
   inquirer
   .prompt([
     {
       name:'item',
       type: 'list',
       choices: function(){
-        var itemArr = ['<--BACK TO DEPARTMENTS'];
+        var itemArr = ['MAIN MENU'];
         var item;
         for (i=0; i<res.length; i++){
-          item = res[i].item_id + " " + res[i].product_name + " $" + res[i].price;
+          item = res[i].item_id + " " + res[i].product_name + " $" + res[i].stock_quantity;
           itemArr.push(item);
         }
         itemArr.push('QUIT'.red);
         return itemArr;
       },
-      message: 'Select an item to add it to your cart\n'.red
+      message: 'Select the item you want to resupply\n'.red
     }
   ])
   .then(function(selection){
-    if(selection.item === '<--BACK TO DEPARTMENTS'){
-      clearScreen();
-      selectDept();
+    if(selection.item === 'MAIN MENU'){
+      mainMenu();
     }
     else if(selection.item === 'QUIT') {process.exit()}
+    else {
+      // get item id 
+      var getItemID = selection.item.split(" ");
+      var itemID = getItemID[0];
+      // add to shopping cart here
+      addToCart(itemID, selection.item, count);
+    };
   });
+
+  var query = connection.query(
+    'UPDATE products SET stock_quantity = (stock_quantity - ?), product_sales = (product_sales + ?) WHERE item_id = ?',
+    [qty, extAmt, id],
+    function(err,res){
+      if(err) throw err;      
+      cartArr = [];
+      itemCount = 0;
+      tempQty = 0;
+      selectDept(-1);
+    }
+  );
+
+
+
+
+}
+
+function addNewProd(){
+  console.log('add new product functionality is under construction');
+  // need to add a menu that asks for product_name --> department_name --> price --> stock_quantity
+  // then does an insert into the products table
+}
+
+function queryItems(dept, operation){
+  var queryResponse;
+  if(operation === 'view'){
+    if(dept === 'ALL'){
+      console.log('SELECT * FROM products')
+      connection.query("SELECT * FROM products", function(err,res){
+        if (err) throw err;
+        displayItems(res, operation);  
+        // console.log('Response from select all is: ' + JSON.stringify(res));
+        // return res;
+        // console.log('queryResonse is: '+queryResponse)
+      });
+    } 
+    else{
+      connection.query("SELECT * FROM products WHERE department_name = ?", dept, function(err,res){
+        if (err) throw err;
+        displayItems(res, operation); 
+        // queryResponse = res;
+      });
+    }
+  }
+  // else if(operation === 'low'){
+  //   if(dept === 'ALL'){
+  //     connection.query("SELECT * FROM products WHERE stock_quantity <= 5", function(err,res){
+  //       if (err) throw err;
+  //       queryResponse = res;
+  //     });
+  //   } 
+  //   else{
+  //     connection.query("SELECT * FROM products WHERE stock_quantity <= 5 AND department_name = ?", dept, function(err,res){
+  //       if (err) throw err;
+  //       queryResponse = res;
+  //     });
+  //   }
+  // }
+  // console.log('reponse in queryItems is: '+res)
+  // return res;
+}
+
+function displayItems(res, operation){
+  displayLogo(operation);
+  var productsArr = [];
+  // ItemObj constructor.  move to its own js file?
+  var ItemObj = function(name, dept, price, SOH, sales){
+    this.product = name;
+    this.department = dept;
+    this.price = price;
+    this.quantity = SOH;
+    this.sales = sales
+  }
+  // fill array with item objects based on query results
+  for(i=0; i<res.length; i++){
+    item = new ItemObj(
+      res[i].product_name, 
+      res[i].department_name, 
+      res[i].price,
+      res[i].stock_quantity,
+      res[i].product_sales
+    )
+    productsArr.push(item);
+  }
+  // format output
+  var columns = columnify(productsArr, {
+    columnSplitter: ' | '
+  });
+  // clearScreen();
+  console.log('\n');
+  // title of report
+  if(operation === 'view'){
+    console.log('**********ALL INVENTORY**********\n');
+  }
+  else if(operation === 'low'){
+    console.log('**********LOW INVENTORY**********\n'); 
+  }
+  //display output
+  console.log(columns);
+  console.log('\n');
+  // pause before returning to main menu
+  pause();
 }
 
 
-displayLogo = function(){
+// function selectItems(res, operation){
+//   displayLogo(operation);
+//   inquirer
+//   .prompt([
+//     {
+//       name:'item',
+//       type: 'list',
+//       choices: function(){
+//         var itemArr = ['<--BACK TO DEPARTMENTS'];
+//         var item;
+//         for (i=0; i<res.length; i++){
+//           item = res[i].item_id + " " + res[i].product_name + " $" + res[i].price + res[i].stock_quantity;
+//           itemArr.push(item);
+//         }
+//         itemArr.push('QUIT'.red);
+//         return itemArr;
+//       },
+//       message: 'Select an item to add inventory\n'.red
+//     }
+//   ])
+//   .then(function(selection){
+//     if(selection.item === '<--BACK TO DEPARTMENTS'){
+//       clearScreen();
+//       selectDept();
+//     }
+//     else if(selection.item === 'QUIT') {process.exit()}
+//     else {
+//       // go to addInventory
+//     }
+//   });
+// }
+
+displayLogo = function(operation){
+  // console.log('OPERATION IS: ' + operation);
   const topleft = cliBoxes.double.topLeft;
   const topright = cliBoxes.double.topRight;
   const bottomleft = cliBoxes.double.bottomLeft;
@@ -123,13 +318,33 @@ displayLogo = function(){
   const companyLine = side.green+'               '+company.bold.yellow+'               '+side.green;
   const sloganLine = side.green+" "+slogan.yellow+" "+side.green;
   const managerLine = side.green + "             MANAGER SCREEN              ".cyan + side.green;
-  clearScreen();
+  var operationLine;
+  if(operation === 'main'){
+    operationLine = side.green +   "               MAIN MANU                 ".cyan + side.green;
+  }
+  else if(operation === 'dept'){
+    operationLine = side.green +   "              DEPARTMENTS                ".cyan + side.green;
+  }
+  else if(operation === 'view'){
+    operationLine = side.green +   "             VIEW INVENTORY              ".cyan + side.green;
+  }
+  else if(operation === 'low'){
+    operationLine = side.green +   "           VIEW LOW INVENTORY            ".cyan + side.green;
+  }
+  else if(operation === 'stock'){
+    operationLine = side.green +   "                RESTOCK                  ".cyan + side.green;
+  }
+  else if(operation === 'new'){
+    operationLine = side.green +   "           ADD NEW PRODUCTS              ".cyan + side.green;
+  }
+  
+  // clearScreen();
   console.log(BorderTop.green);
   console.log(BorderSides.green);
   console.log(companyLine.bold);
   console.log(sloganLine);
-  console.log(BorderSides.green);
   console.log(managerLine);
+  console.log(operationLine);
   console.log(BorderSides.green);
   console.log(BorderBottom.green);
 }
@@ -137,4 +352,19 @@ displayLogo = function(){
 function clearScreen(){
   // clear output from this application/prevent scrolling
   process.stdout.write('\033c'); 
+}
+
+function pause(){
+  inquirer
+  .prompt([
+    {
+      name:'input',
+      type: 'list',
+      choices: ['<ENTER>'],
+      message: 'Hit ENTER to return to the main menu'
+    }
+  ])
+  .then(function(selection){
+    mainMenu();
+  });
 }
